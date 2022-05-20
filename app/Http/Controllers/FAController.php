c9e6cfcf3e354c15766dfc3bd600d73882edc8ea
Sokheng27 <nysokhengkhmer@gmail.com>
2022-05-20 20:34:10 +0700

    change message

--- app/Http/Controllers/FAController.php
+++ app/Http/Controllers/FAController.php
@@ -86,7 +86,7 @@ class FAController extends Controller
         if ($isDFA == true) {
             return $this->ok(new FACollection(FA::where('id', $request->fa_id)->get()), "Deterministic Finite Automaton (DFA)");
         } elseif ($isDFA == false) {
-            return $this->fail(new FACollection(FA::where('id', $request->fa_id)->get()),"Nondeterministic Finite Automaton (DFA)");
+            return $this->fail(new FACollection(FA::where('id', $request->fa_id)->get()),"Nondeterministic Finite Automaton (NFA)");
         }
         return $isDFA;
     }
