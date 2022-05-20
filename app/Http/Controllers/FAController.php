a84fef1d1347fa222796f99b8fa6be8985e83aba
Sokheng27 <nysokhengkhmer@gmail.com>
2022-05-21 00:10:42 +0700

    fix

--- app/Http/Controllers/FAController.php
+++ app/Http/Controllers/FAController.php
@@ -61,7 +61,7 @@ class FAController extends Controller
             $fa = FA::where('id', $request->fa_id)->first();
 
             // Check if symbol include ε
-            $check_symbol = in_array(' ', json_decode($fa->symbol));
+            $check_symbol = in_array("", json_decode($fa->symbol));
 
             if ($check_symbol == true) {
                 $isDFA = false;
